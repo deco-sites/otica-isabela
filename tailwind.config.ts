04cede6c1a757decae4b598e1133a5ad448f51d1
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-07-18 00:15:44 -0300

    added new icons and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,17 @@
 import daisyui from "daisyui";
 
+
 export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: {
+    container: { center: true },
+    extend: {
+      colors: {
+        "bg-blue-200": "#42c3ff",
+        "success":" #00ff2a"
+      },
+    },
+  },
 };
