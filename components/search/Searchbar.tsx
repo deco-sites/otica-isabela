69d8238b6b9ee0c1536cdaf6d3b9311e98d55bb9
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-07-27 19:18:46 -0300

    finished task

--- components/search/Searchbar.tsx
+++ components/search/Searchbar.tsx
@@ -14,7 +14,6 @@ import Icon from "$store/components/ui/Icon.tsx";
 import { useAutocomplete } from "deco-sites/std/packs/vtex/hooks/useAutocomplete.ts";
 import { sendEvent } from "$store/sdk/analytics.tsx";
 import { formatPrice } from "$store/sdk/format.ts";
-import Spinner from "$store/components/ui/Spinner.tsx";
 
 // Editable props
 export interface EditableProps {
@@ -65,17 +64,17 @@ function Searchbar({
 
   return (
     <>
-      <div class="flex bg-white w-full h-10  rounded-lg">
+      <div class="flex bg-white w-full h-10 items-center justify-center  rounded-lg">
         <div class="flex items-center w-full ">
           <form
             id="searchbar"
             action={action}
-            class="group flex flex-row justify-center items-center border border-none w-full"
+            class="group flex flex-row justify-between items-center border border-none w-full pr-1 pl-3"
           >
             <input
               ref={searchInputRef}
               id="search-input"
-              class="flex outline-none placeholder-shown:sibling:hidden font-extralight text-sm text-start p-2 w-4/5"
+              class="flex outline-none placeholder-shown:sibling:hidden font-extralight text-sm text-start w-4/5"
               name={name}
               defaultValue={query}
               onInput={(e) => {
@@ -104,26 +103,40 @@ function Searchbar({
               strokeWidth={0.01}
             />
           </form>
-          {loading.value && <Spinner />}
+        </div>
+        <div class="flex items-center justify-end w-5 relative pr-1">
+          {loading.value && (
+            <img
+              alt="Loading Gif"
+              width="16"
+              height="16"
+              src="/gif/loadingGif.gif"
+            />
+          )}
         </div>
       </div>
       {hasProducts && (
         <div class="relative w-full">
           <div class="absolute flex flex-col top-2 left-0 bg-white border border-blue-200 rounded-lg  w-full p-4 z-50">
-            <h1 class="text-base text-secondary mb-5">Sugestões de óculos</h1>
+            <h1 class="text-base text-black font-normal mb-5">
+              Sugestões de óculos
+            </h1>
             <div class=" flex flex-wrap  w-full gap-y-4 ">
               {suggestionProducts?.map(({ image, name, url, offers }) => {
-                image?.[0].url;
                 return (
                   <a
                     src={url}
                     class="w-1/2 flex flex-col justify-center items-center text-center "
                   >
-                    <img src={image?.[0].url ?? ""} width={100} />
-                    <span class="text-secondary font-bold text-xs line-clamp-3">
+                    <img
+                      src={image?.[0].url ?? ""}
+                      alt={image?.[0].description ?? ""}
+                      width={100}
+                    />
+                    <span class="text-black font-bold text-xs line-clamp-3">
                       {name}
                     </span>
-                    <span class="text-xs text-blue-200 font-medium mt-3">
+                    <span class="text-xs text-blue-200 font-bold mt-3">
                       {formatPrice(offers?.highPrice)}
                     </span>
                   </a>
