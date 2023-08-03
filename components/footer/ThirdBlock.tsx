c283036f52d21d265be5f980cd7ed7107808bf09
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-08-02 21:09:08 -0300

    update date

--- components/footer/ThirdBlock.tsx
+++ components/footer/ThirdBlock.tsx
@@ -12,7 +12,7 @@ export const ThirdBlock = (
     {};
 
   const date = new Date();
-  const day = String(date.getDate()).padStart(2, "0");
+  const day = String(date.getDate() - 1).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const currentDate = `${day}/${month}/${date.getFullYear()}`;
 
