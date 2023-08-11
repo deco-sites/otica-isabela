61ff06b915d732a19ddc99cefa996d6317f57822
weslybrandao <brandaowesly@gmail.com>
2023-08-11 15:29:55 -0300

    code adjusts

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -1,6 +1,8 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { LiveConfig, LiveState } from "$live/types.ts";
 import { getCookies } from "std/http/mod.ts";
+import { ISABELA_DIAS_COOKIE } from "$store/packs/constants.ts";
+import { Manifest } from "$store/live.gen.ts";
 
 const setCookie = (
   sessionKey: string,
@@ -17,29 +19,28 @@ const setCookie = (
 
 export const handler = async (
   req: Request,
-  ctx: MiddlewareHandlerContext<LiveConfig<LiveState>>,
+  ctx: MiddlewareHandlerContext<LiveConfig<unknown, LiveState, Manifest>>,
 ) => {
   const res = await ctx.next();
   const cookies = getCookies(req.headers);
-  const sessionToken = cookies["IsabelaDias_SessionCustomerKey"] ?? false;
+  const sessionToken = cookies[ISABELA_DIAS_COOKIE] ?? false;
   const IdCustomer = false; // fake data to finish component functionality
-  const date = new Date();
-  const expirationDate = date.setFullYear(date.getFullYear() + 1);
-
-  // Aguardando a correção da API de session para chamar a API.
-
-  // const authApiResponse = await ctx.state.invoke(
-  //   "$store/loaders/store/session.ts",
-  //   {},
-  // );
 
   // console.log("Retorno da api", authApiResponse);
 
   if (!sessionToken) {
+    // Aguardando a correção da API de session para chamar a API.
+
+    // const authApiResponse = await ctx.state.invoke(
+    //   "deco-sites/otica-isabela/loaders/store/session.ts",
+    // );
+
+    // console.log("eae", authApiResponse);
+
     setCookie(
       "9af13ceec92746088dfa0880d25a7c6e",
       res,
-      IdCustomer ? expirationDate : null,
+      IdCustomer ? new Date().setFullYear(new Date().getFullYear() + 1) : null,
     );
   }
 
