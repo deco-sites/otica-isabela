4623183b3091771e1207472357ddb2c051259cfb
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-08-15 00:54:21 -0300

    added mock

--- components/ui/TestimonialItem.tsx
+++ components/ui/TestimonialItem.tsx
@@ -18,32 +18,52 @@ export interface Testimonial {
   };
 }
 
-const TestimonialItem = ({ review, user }: Testimonial) => {
-  const { locale, name } = user ?? {};
-  const { description, image, ratting } = review ?? {};
+interface Review {
+  ratingValue: number;
+  authorName: string;
+  reviewDescription: string;
+  authorCity: string;
+  productName: string;
+  productPhoto: string;
+  productLink: string;
+  additionalImage: string;
+  membershipBadge?: string;
+}
 
+const TestimonialItem = (
+  {
+    additionalImage,
+    authorCity,
+    authorName,
+    productLink,
+    productName,
+    productPhoto,
+    ratingValue,
+    reviewDescription,
+    membershipBadge,
+  }: Review,
+) => {
   return (
     <div class="flex flex-col items-center text-center border border-blue-300 rounded-xl px-3 py-5 ">
       <div class="flex items-start justify-center gap-x-2 ">
         <div class="w-1/3 flex flex-col">
-          {image?.src && (
-            <Image
-              src={image.src}
-              alt={image.alt}
-              width={140}
-              class="rounded-xl"
-              height={200}
-            />
-          )}
+          <Image
+            src={additionalImage}
+            alt={productName}
+            width={140}
+            class="rounded-xl"
+            height={200}
+          />
+
           <div class="flex flex-col items-start">
             <span class="text-xs text-blue-200 flex items-center justify-center gap-x-2 font-semibold mt-3 ">
               <Icon width={18} height={22} id="Locale" />
-              {locale}
+              {authorCity}
             </span>
-            <p class="text-xs font-semibold ">{name}</p>
-            {!!ratting && ratting <= 5 && (
+            <p class="text-xs font-semibold ">{authorName}</p>
+            {!!ratingValue && ratingValue <= 5 && (
               <div class="flex">
-                {Array.from({ length: ratting }).map((_, index) => (
+                {Array.from({ length: ratingValue }).map((_, index) => (
                   <Icon
                     key={`ratingStar${index}`}
                     width={21}
@@ -55,22 +75,21 @@ const TestimonialItem = ({ review, user }: Testimonial) => {
             )}
           </div>
         </div>
-        <a href="productsLink" class="w-2/3 flex flex-col items-center">
+        <a href={productLink} class="w-2/3 flex flex-col items-center">
           <p class="text-sm text-start font-normal text-black border-b border-b-blue-300 pb-8">
-            {review}
+            {reviewDescription}
           </p>
           <div class="flex flex-col justify-center items-center ">
             <span class="my-4 font-semibold text-base text-black underline ">
-              {description}
+              {productName}
             </span>
-            {image?.src && (
-              <Image
-                src={image.src}
-                alt={image?.alt}
-                width={150}
-                height={60}
-              />
-            )}
+
+            <Image
+              src={productPhoto}
+              alt={productName}
+              width={150}
+              height={60}
+            />
           </div>
         </a>
       </div>
--- sections/Content/Testimonials.tsx
+++ sections/Content/Testimonials.tsx
@@ -4,18 +4,111 @@ import Slider from "$store/components/ui/Slider.tsx";
 import SliderJS from "$store/islands/SliderJS.tsx";
 import { useId } from "preact/hooks";
 import TestimonialItem from "$store/components/ui/TestimonialItem.tsx";
-import type { Testimonial } from "$store/components/ui/TestimonialItem.tsx";
 import { ProductShelfDots } from "$store/components/product/ProductShelf.tsx";
+
+interface Review {
+  ratingValue: number;
+  authorName: string;
+  reviewDescription: string;
+  authorCity: string;
+  productName: string;
+  productPhoto: string;
+  productLink: string;
+  additionalImage: string;
+  membershipBadge?: string;
+}
 
 export interface Props {
   header?: IconTitleProps;
-  testimonials?: Testimonial[];
 }
 
 export default function Testimonials(
-  { header, testimonials }: Props,
+  { header }: Props,
 ) {
   const id = useId();
+
+  const reviewMock: Review[] = [
+    {
+      additionalImage:
+        "https://www.oticaisabeladias.com.br//Images/Blob/Avaliacoes//876231b79dc743c1860a4b7e8d3d8cc7.webp",
+      authorCity: "Florianópolis - SC",
+      authorName: "Maria Fernanda",
+      productLink:
+        "/produto/Armacao-Oculos-Clipon-Grau-Feminino-1271-Isabela-Dias",
+      productName: "Óculos Clipon Redondo Feminino 1271",
+      productPhoto:
+        "https://www.oticaisabeladias.com.br/backoffice//Images/Blob/webp_id//53f3483dfa624952aff955a695e069d9-2.webp",
+      ratingValue: 5,
+      reviewDescription:
+        "Os óculos chegaram bem rápido (pedi Sedex) e bem embalados, o que era uma das minhas maiores preocupações. Eles foram elogiados pela pessoa mais importantes, minha oftalmologista! Ela disse que ficaram muito bons e realmente, mesmo sendo um grau maior minha lente ficou mais fina que a anterior. As lentes clip-on são muito estilosas também. Recomendo 100%",
+      membershipBadge:
+        "https://www.oticaisabeladias.com.br/content/assets/images/gold.png",
+    },
+    {
+      additionalImage:
+        "https://www.oticaisabeladias.com.br//Images/Blob/Avaliacoes//876231b79dc743c1860a4b7e8d3d8cc7.webp",
+      authorCity: "Florianópolis - SC",
+      authorName: "Maria Fernanda",
+      productLink:
+        "/produto/Armacao-Oculos-Clipon-Grau-Feminino-1271-Isabela-Dias",
+      productName: "Óculos Clipon Redondo Feminino 1271",
+      productPhoto:
+        "https://www.oticaisabeladias.com.br/backoffice//Images/Blob/webp_id//53f3483dfa624952aff955a695e069d9-2.webp",
+      ratingValue: 5,
+      reviewDescription:
+        "Os óculos chegaram bem rápido (pedi Sedex) e bem embalados, o que era uma das minhas maiores preocupações. Eles foram elogiados pela pessoa mais importantes, minha oftalmologista! Ela disse que ficaram muito bons e realmente, mesmo sendo um grau maior minha lente ficou mais fina que a anterior. As lentes clip-on são muito estilosas também. Recomendo 100%",
+      membershipBadge:
+        "https://www.oticaisabeladias.com.br/content/assets/images/gold.png",
+    },
+    {
+      additionalImage:
+        "https://www.oticaisabeladias.com.br//Images/Blob/Avaliacoes//876231b79dc743c1860a4b7e8d3d8cc7.webp",
+      authorCity: "Florianópolis - SC",
+      authorName: "Maria Fernanda",
+      productLink:
+        "/produto/Armacao-Oculos-Clipon-Grau-Feminino-1271-Isabela-Dias",
+      productName: "Óculos Clipon Redondo Feminino 1271",
+      productPhoto:
+        "https://www.oticaisabeladias.com.br/backoffice//Images/Blob/webp_id//53f3483dfa624952aff955a695e069d9-2.webp",
+      ratingValue: 5,
+      reviewDescription:
+        "Os óculos chegaram bem rápido (pedi Sedex) e bem embalados, o que era uma das minhas maiores preocupações. Eles foram elogiados pela pessoa mais importantes, minha oftalmologista! Ela disse que ficaram muito bons e realmente, mesmo sendo um grau maior minha lente ficou mais fina que a anterior. As lentes clip-on são muito estilosas também. Recomendo 100%",
+      membershipBadge:
+        "https://www.oticaisabeladias.com.br/content/assets/images/gold.png",
+    },
+    {
+      additionalImage:
+        "https://www.oticaisabeladias.com.br//Images/Blob/Avaliacoes//876231b79dc743c1860a4b7e8d3d8cc7.webp",
+      authorCity: "Florianópolis - SC",
+      authorName: "Maria Fernanda",
+      productLink:
+        "/produto/Armacao-Oculos-Clipon-Grau-Feminino-1271-Isabela-Dias",
+      productName: "Óculos Clipon Redondo Feminino 1271",
+      productPhoto:
+        "https://www.oticaisabeladias.com.br/backoffice//Images/Blob/webp_id//53f3483dfa624952aff955a695e069d9-2.webp",
+      ratingValue: 5,
+      reviewDescription:
+        "Os óculos chegaram bem rápido (pedi Sedex) e bem embalados, o que era uma das minhas maiores preocupações. Eles foram elogiados pela pessoa mais importantes, minha oftalmologista! Ela disse que ficaram muito bons e realmente, mesmo sendo um grau maior minha lente ficou mais fina que a anterior. As lentes clip-on são muito estilosas também. Recomendo 100%",
+      membershipBadge:
+        "https://www.oticaisabeladias.com.br/content/assets/images/gold.png",
+    },
+    {
+      additionalImage:
+        "https://www.oticaisabeladias.com.br//Images/Blob/Avaliacoes//876231b79dc743c1860a4b7e8d3d8cc7.webp",
+      authorCity: "Florianópolis - SC",
+      authorName: "Maria Fernanda",
+      productLink:
+        "/produto/Armacao-Oculos-Clipon-Grau-Feminino-1271-Isabela-Dias",
+      productName: "Óculos Clipon Redondo Feminino 1271",
+      productPhoto:
+        "https://www.oticaisabeladias.com.br/backoffice//Images/Blob/webp_id//53f3483dfa624952aff955a695e069d9-2.webp",
+      ratingValue: 5,
+      reviewDescription:
+        "Os óculos chegaram bem rápido (pedi Sedex) e bem embalados, o que era uma das minhas maiores preocupações. Eles foram elogiados pela pessoa mais importantes, minha oftalmologista! Ela disse que ficaram muito bons e realmente, mesmo sendo um grau maior minha lente ficou mais fina que a anterior. As lentes clip-on são muito estilosas também. Recomendo 100%",
+      membershipBadge:
+        "https://www.oticaisabeladias.com.br/content/assets/images/gold.png",
+    },
+  ];
 
   return (
     <>
@@ -27,7 +120,7 @@ export default function Testimonials(
           id={id}
         >
           <Slider class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5 w-full">
-            {testimonials?.map((review, index) => (
+            {reviewMock?.map((review, index) => (
               <Slider.Item
                 index={index}
                 class="flex flex-col gap-4 carousel-item w-full"
@@ -38,7 +131,7 @@ export default function Testimonials(
           </Slider>
 
           <div class="flex flex-row w-full gap-x-3 justify-center items-center py-14 ">
-            {testimonials?.map((_, index) => (
+            {reviewMock?.map((_, index) => (
               <Slider.Dot index={index}>
                 {ProductShelfDots}
               </Slider.Dot>
