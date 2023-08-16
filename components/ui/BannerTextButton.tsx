4728fdf02739a6fc87f9ae51be71e998353ef271
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-08-15 21:25:02 -0300

    resolved bugs

--- components/ui/BannerTextButton.tsx
+++ components/ui/BannerTextButton.tsx
@@ -8,7 +8,7 @@ export interface BannerTextButton {
   text: string;
   /** @description href where the button will send to */
   href?: string;
-  /** @description justify and align the text (MOBILE ONLY)*/
+  /** @description justify and align the text*/
   textAlign?: "center" | "left";
   image: {
     /** @description Image for big screens */
@@ -16,9 +16,9 @@ export interface BannerTextButton {
     /** @description Image for small screens */
     mobile: LiveImage;
     /** @description image alt text */
-    alt?: string;
-    /** @description image align (DESKTOP ONLY)*/
-    imageAlign?: "right" | "left";
+    alt: string;
+    /** @description image align */
+    imageAlign?: { desktop: "right" | "left"; mobile: "top" | "botton" };
   };
 }
 
@@ -26,14 +26,16 @@ function BannerTextButton(
   { text, href, textAlign, title, image }: BannerTextButton,
 ) {
   return (
-    <div class=" container flex  flex-col-reverse lg:flex-row my-5 ">
+    <div
+      class={` container flex  ${
+        image?.imageAlign?.mobile === "top" ? "flex-col" : "flex-col-reverse"
+      } lg:flex-row my-5`}
+    >
       <div
-        class={`flex text-center mt-4 lg:mt-0 flex-col justify-center items-center w-full lg:w-1/3 ${
-          textAlign === "left"
-            ? "pr-12 pl-4 items-start"
-            : "pr-4 pl-12 items-end"
+        class={`flex text-center px-3 lg:px-0 lg:mt-0 flex-col justify-center items-center w-full lg:w-1/3 ${
+          textAlign === "left" ? "pr-12 text-start" : "pr-4  items-end"
         } ${
-          image?.imageAlign === "left"
+          image?.imageAlign?.desktop === "left"
             ? "lg:pl-12 lg:pr-4 lg:order-1 lg:items-start"
             : "lg:pr-12 lg:pl-4 lg:items-end"
         } `}
@@ -57,18 +59,20 @@ function BannerTextButton(
         >
           <Source
             src={image?.mobile}
-            width={690}
+            width={390}
+            height={220}
             media="(max-width: 983px)"
           />
           <Source
             src={image?.desktop}
             width={730}
+            height={410}
             media="(max-width: 984px)"
           />
           <img
-            class="w-full rounded-3xl"
-            src={image?.desktop}
-            alt={image?.alt ?? title}
+            class="w-full rounded-lg"
+            src={image?.mobile ?? image?.desktop}
+            alt={image.alt}
           />
         </Picture>
       </a>
