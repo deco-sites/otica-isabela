2c5602c73c935138e45cddd8ff959f3cd232f120
Rodrigo Barreto da Cruz <80490671+Rodrigo-Barreto@users.noreply.github.com>
2023-08-18 13:11:16 -0300

    Update components/product/ProductShelf.tsx
    Co-authored-by: gustavo-vasconcellos <pvasconcellos.gustavo@gmail.com>

--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -77,7 +77,7 @@ function ProductShelf({
         <div class="flex flex-row w-full gap-x-3 justify-center items-center py-14 ">
           {products.map((_, index) => (
             <Slider.Dot index={index}>
-              {ProductShelfDots}
+              <ProductShelfDots />
             </Slider.Dot>
           ))}
         </div>
