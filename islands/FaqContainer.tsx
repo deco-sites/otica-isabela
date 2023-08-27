e8d13fbb74de168058fd23645d1ea6de351cc965
Rodrigo-Barreto <rodicruz10@gmail.com>
2023-08-27 19:55:48 -0300

    feat: update

---
+++ islands/FaqContainer.tsx
@@ -0,0 +1,22 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import type { ComponentChildren } from "preact";
+
+const FaqContainer = (
+  { backgroundColor, children }: {
+    backgroundColor?: string;
+    children?: ComponentChildren;
+  },
+) => {
+  const isDesktop = window?.matchMedia?.("(min-width: 984px)")?.matches;
+
+  return (
+    <div
+      style={{ backgroundColor: isDesktop ? backgroundColor : "#ffffff" }}
+      class="w-full flex items-center justify-center px-4 lg:px-0  "
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FaqContainer;
