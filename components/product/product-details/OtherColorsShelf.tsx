5e60bf38749a42a1ddb53f1af31adc18cbfec0b8
Felipe Ireslan da Silva <felipe.ireslan-ext@ab-inbev.com>
2023-10-11 20:18:24 -0300

    fix dots and centralize images vertically

--- components/product/product-details/OtherColorsShelf.tsx
+++ components/product/product-details/OtherColorsShelf.tsx
@@ -15,12 +15,21 @@ function OtherColorsShelf({ product }: Props) {
   const { isVariantOf, sku } = product;
   const { hasVariant } = isVariantOf || {};
 
-  const images = hasVariant?.map((variant: Variant) => ({
-    image: variant.Imagem,
-    alternateName: variant.alternateName,
-    url: variant.url,
-    sku: variant.sku,
-  }));
+  const images = hasVariant
+    ?.map((variant: Variant) => ({
+      image: variant.Imagem,
+      alternateName: variant.alternateName,
+      url: variant.url,
+      sku: variant.sku,
+    }))
+    .filter((variant) => variant.sku !== sku);
+
+  const dots = Array.from(
+    { length: Math.ceil(images?.length! / 3) },
+    (_, index) => index
+  );
+
+  if (!images) return null;
 
   return (
     <div class="mt-10">
@@ -30,34 +39,31 @@ function OtherColorsShelf({ product }: Props) {
       <div class="w-full flex flex-col gap-0 md:gap-12 lg:gap-16 mt-10">
         <div id={id} class="container flex flex-col px-0 sm:px-5">
           <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
-            {images?.map(
-              ({ image, alternateName, url, sku: variantSku }, index) =>
-                variantSku !== sku && (
-                  <div class="flex flex-col">
-                    <Slider.Item
-                      index={index}
-                      class="carousel-item w-full lg:first:pl-0 first:pl-4 last:pr-4  lg:last:pr-0 justify-center"
-                    >
-                      <a href={url} class="hidden md:block">
-                        <img
-                          src={image}
-                          alt={alternateName}
-                          width="340px"
-                          height="190px"
-                        />
-                      </a>
-                      <a href={url} class="md:hidden">
-                        <img
-                          src={image}
-                          alt={alternateName}
-                          width="260px"
-                          height="190px"
-                        />
-                      </a>
-                    </Slider.Item>
-                  </div>
-                )
-            )}
+            {images?.map(({ image, alternateName, url }, index) => (
+              <div class="flex flex-col">
+                <Slider.Item
+                  index={index}
+                  class="carousel-item w-full lg:first:pl-0 first:pl-4 last:pr-4  lg:last:pr-0 justify-center items-center"
+                >
+                  <a href={url} class="hidden md:block">
+                    <img
+                      src={image}
+                      alt={alternateName}
+                      width="340px"
+                      height="190px"
+                    />
+                  </a>
+                  <a href={url} class="md:hidden">
+                    <img
+                      src={image}
+                      alt={alternateName}
+                      width="260px"
+                      height="190px"
+                    />
+                  </a>
+                </Slider.Item>
+              </div>
+            ))}
           </Slider>
 
           <SliderJS
@@ -69,7 +75,15 @@ function OtherColorsShelf({ product }: Props) {
             perPageDots
           />
 
-          <div class="flex flex-row w-full gap-x-3 justify-center items-center py-14 ">
+          {/* Desktop - Dots */}
+          <div class="hidden lg:flex flex-row w-full gap-x-3 justify-center items-center py-14">
+            {dots?.map((_, index) => (
+              <Slider.Dot index={index} />
+            ))}
+          </div>
+
+          {/* Mobile - Dots */}
+          <div class="flex flex-row w-full gap-x-3 justify-center items-center py-14 lg:hidden">
             {images?.map((_, index) => (
               <Slider.Dot index={index} />
             ))}
