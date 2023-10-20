c1bcf8a278fd815d4e077eabdcdf8685b6d70cfa
Felipe Ireslan da Silva <felipe.ireslan-ext@ab-inbev.com>
2023-10-20 19:24:01 -0300

    final adjustments on product review mobile/desktop

--- components/product/product-details/Review/ImagesModal.tsx
+++ components/product/product-details/Review/ImagesModal.tsx
@@ -19,7 +19,7 @@ function ImagesModal({ reviews, selected }: Props) {
     rating: selected.ratingValue,
     description: selected.reviewDescription,
   });
-  const isOpened = useSignal(false);
+  const isOpen = useSignal(false);
   const id = `review-image-slider-modal-${useId()}`;
 
   const setSelectedReview = (review: Review) => {
@@ -30,69 +30,81 @@ function ImagesModal({ reviews, selected }: Props) {
     };
   };
 
-  return (
-    <Modal
-      class="p-0 w-full rounded-[10px] relative"
-      inputClasses="absolute w-full h-full"
-      open={isOpened.value}
-    >
-      <div
-        class="absolute right-0 z-10 bg-black rounded-[10px] p-1"
-        onClick={() => isOpened.value = false}
-      >
-        <Icon id="XMark" size={20} fill="white" />
-      </div>
-      <div id="image-container" class="relative">
-        <Image
-          class="w-full"
-          src={selectedReview.value.image}
-          width={314}
-          height={410}
-        />
-        <div class="absolute bottom-0 w-full p-[10px] bg-[rgba(0,0,0,.5)]">
-          <Ratings ratingValue={selectedReview.value.rating} />
-          <p class="mt-5 text-[13px] text-white">
-            {selectedReview.value.description}
-          </p>
-        </div>
-      </div>
-      <div
-        id={id}
-        class="container flex flex-col px-0 bg-[rgba(0,0,0,.5)] p-[15px]"
-      >
-        <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
-          {reviews?.map((review, index) => (
-            review.additionalImage &&
-            (
-              <div class="flex flex-col">
-                <Slider.Item
-                  index={index}
-                  class="carousel-item w-full first:pl-4 last:pr-4 justify-center lg:first:pl-0 lg:last:pr-0"
-                >
-                  <Image
-                    class="rounded-[9px]"
-                    src={review.additionalImage}
-                    alt={review.reviewDescription}
-                    width={157}
-                    height={205}
-                    onClick={() => setSelectedReview(review)}
-                  />
-                </Slider.Item>
-              </div>
-            )
-          ))}
-        </Slider>
+  const toggleModal = () => {
+    isOpen.value = !isOpen.value;
+    setSelectedReview(selected);
+  };
 
-        <SliderJS
-          itemsPerPage={{
-            desktop: { 0: 4 },
-            mobile: { 0: 1.5 },
-          }}
-          rootId={id}
-          perPageDots
-        />
+  return (
+    <>
+      <div
+        id="modal-trigger"
+        class="absolute w-full h-full"
+        onClick={toggleModal}
+      >
       </div>
-    </Modal>
+      <Modal
+        class="p-0 w-full rounded-[10px] max-w-[314px] lg:max-w-[565px]"
+        open={isOpen.value}
+        onClose={toggleModal}
+      >
+        <div
+          class="absolute right-0 z-10 bg-black rounded-[10px] p-[10px]"
+          onClick={toggleModal}
+        >
+          <Icon id="XMark" size={10} fill="white" />
+        </div>
+        <div id="image-container" class="relative">
+          <Image
+            class="w-full cursor-pointer"
+            src={selectedReview.value.image}
+            width={314}
+            height={410}
+          />
+          <div class="absolute bottom-0 w-full p-[10px] bg-[rgba(0,0,0,.5)]">
+            <Ratings ratingValue={selectedReview.value.rating} />
+            <p class="mt-5 text-[13px] text-white">
+              {selectedReview.value.description}
+            </p>
+          </div>
+        </div>
+        <div
+          id={id}
+          class="container flex flex-col px-0 bg-[rgba(0,0,0,.5)] p-[15px] lg:px-[15px]"
+        >
+          <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
+            {reviews?.map((review, index) => (
+              review.additionalImage &&
+              (
+                <div class="flex flex-col">
+                  <Slider.Item
+                    index={index}
+                    class="carousel-item w-full first:pl-4 last:pr-4 justify-center lg:first:pl-0 lg:last:pr-0"
+                  >
+                    <Image
+                      class="rounded-[9px]"
+                      src={review.additionalImage}
+                      alt={review.reviewDescription}
+                      width={157}
+                      height={205}
+                      onClick={() => setSelectedReview(review)}
+                    />
+                  </Slider.Item>
+                </div>
+              )
+            ))}
+          </Slider>
+
+          <SliderJS
+            itemsPerPage={{
+              desktop: { 0: 3 },
+              mobile: { 0: 1.5 },
+            }}
+            rootId={id}
+          />
+        </div>
+      </Modal>
+    </>
   );
 }
 
--- components/product/product-details/Review/Index.tsx
+++ components/product/product-details/Review/Index.tsx
@@ -30,66 +30,71 @@ function Review({ page, memberImages }: Props) {
   const reviewsWithImages = reviews.filter((review) => review.additionalImage);
 
   return (
-    <div class="mt-12">
+    <>
       {/* Overview */}
-      <div id="title-and-rating" class="bg-[#f8f8f8] py-[70px]">
-        <h1 class="text-xl font-bold text-center px-[15px] md:text-2xl">
-          Nos sentimentos felizes por fazer a diferença na saúde visual de
-          milhares de pessoas
-        </h1>
-        <div class="flex justify-center items-center gap-1">
-          <h1 class="text-[64px] font-bold text-orange-500">
-            {generalRate.toFixed(1).replace(".", ",")}
-          </h1>
-          <Icon id="Ratings" size={39} />
-        </div>
-        <h1 class="text-lg text-center">Pontuação geral</h1>
+      <div class="mt-12 bg-[#f8f8f8]">
+        <div class="container">
+          <div id="title-and-rating" class="py-[70px]">
+            <h1 class="text-xl text-black font-bold text-center px-[15px] md:text-2xl lg:w-[75%] lg:text-[32px] lg:mx-auto lg:mb-[20px]">
+              Nos sentimos felizes por fazer a diferença na saúde visual de
+              milhares de pessoas
+            </h1>
+            <div class="flex justify-center items-center gap-1">
+              <h1 class="text-[64px] font-bold text-orange-500">
+                {generalRate.toFixed(1).replace(".", ",")}
+              </h1>
+              <Icon id="Ratings" size={39} />
+            </div>
+            <h1 class="text-lg text-center">Pontuação geral</h1>
 
-        {/* Images */}
-        <div class="w-full flex flex-col gap-0 md:gap-12 lg:gap-16 mt-8">
-          <div id={id} class="container flex flex-col px-0 sm:px-5">
-            <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
-              {reviews?.map((review, index) => (
-                review.additionalImage &&
-                (
-                  <div class="flex flex-col relative">
-                    <Slider.Item
-                      index={index}
-                      class="carousel-item w-full first:pl-4 last:pr-4 justify-center lg:first:pl-0 lg:last:pr-0"
-                    >
-                      <Image
-                        class="rounded-[9px]"
-                        src={review.additionalImage}
-                        alt={review.reviewDescription}
-                        width={157}
-                        height={205}
-                      />
-                    </Slider.Item>
+            {/* Images */}
+            <div class="w-full flex flex-col gap-0 md:gap-12 lg:gap-16 mt-8">
+              <div id={id} class="container flex flex-col px-0 sm:px-5">
+                <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
+                  {reviews?.map((review, index) => (
+                    review.additionalImage &&
+                    (
+                      <div class="flex flex-col relative">
+                        <Slider.Item
+                          index={index}
+                          class="carousel-item w-full justify-center"
+                        >
+                          <Image
+                            class="rounded-[9px] cursor-pointer"
+                            src={review.additionalImage}
+                            alt={review.reviewDescription}
+                            width={157}
+                            height={205}
+                          />
+                        </Slider.Item>
 
-                    {/* Modal */}
-                    <ReviewImagesModal
-                      reviews={reviewsWithImages}
-                      selected={review}
-                    />
-                  </div>
-                )
-              ))}
-            </Slider>
+                        {/* Modal */}
+                        <ReviewImagesModal
+                          reviews={reviewsWithImages}
+                          selected={review}
+                        />
+                      </div>
+                    )
+                  ))}
+                </Slider>
 
-            <SliderJS
-              itemsPerPage={{
-                desktop: { 0: 4 },
-                mobile: { 0: 1.5 },
-              }}
-              rootId={id}
-              perPageDots
-            />
+                <SliderJS
+                  itemsPerPage={{
+                    desktop: { 0: 4 },
+                    mobile: { 0: 1.5 },
+                  }}
+                  rootId={id}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
       {/* Feedbacks */}
-      <div id="reviews" class="flex flex-col gap-8 my-12 px-[15px]">
+      <div
+        id="reviews"
+        class="flex flex-col gap-8 my-12 px-[15px] bg-white container"
+      >
         {/* First Review */}
         <Feedback
           review={reviews?.[0]!}
@@ -112,7 +117,7 @@ function Review({ page, memberImages }: Props) {
           />
         )}
       </div>
-    </div>
+    </>
   );
 }
 
