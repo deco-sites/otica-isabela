2789efdc68fff7a0a73807a5efb4a814a889f1d8
SACCI <saccilucas@gmail.com>
2023-10-23 22:37:50 -0300

    fix(packs): double requisition in middleware removed

--- packs/types.ts
+++ packs/types.ts
@@ -2,37 +2,40 @@ import type { Filter, ListItem, Seo } from "apps/commerce/types.ts";
 
 export type Session = {
   SessionCustomer: SessionCustomer;
+  SessionCustomerProducts: string
+  customer: string
+  QtdeProducts: number
 };
 
 export type SessionCustomer = {
-  Company: string;
-  Customer: string;
-  SessionCustomer_CartProduct: unknown[];
-  IdSessionCustomer: number;
-  IdCustomer: number;
-  SessionKey: string;
-  DateAdd: string;
-  DateModified: string;
-  IdCompany: number;
-  Deadline: number;
-  PrazoEntrega: number;
-  ValueFreight: number;
-  PostalCode: string;
-  IdTypeFreight: number;
-  IpPublic: string;
-  IpTracking: string;
-  HostNameTracking: string;
-  CityTracing: string;
-  RegionTracking: string;
-  CountryTracking: string;
-  LOCTracking: string;
-  PostalCodeTracking: string;
-  DateTracking: string;
-  Foto: string;
-  MedidasFotoTracker: string;
-  EmailRecuperacaoCarrinho: boolean;
-  EmailRecuperacaoCompra: boolean;
-  SourceCliente: string;
+  IdSessionCustomer: number
+  IdCustomer: string
+  Customer: string
+  SessionKey: string
+  DateAdd: string
+  DateModified: string
+  IdCompany: number
+  Company: string
+  Deadline: number
+  PrazoEntrega: string
+  ValueFreight: number
+  PostalCode: string
+  IdTypeFreight: number
+  IpPublic: string
+  IpTracking: string
+  HostNameTracking: string
+  CityTracing: string
+  RegionTracking: string
+  CountryTracking: string
+  LOCTracking: string
+  PostalCodeTracking: string
+  DateTracking: string
+  Foto: string
+  MedidasFotoTracker: string
+  EmailRecuperacaoCarrinho: boolean
+  EmailRecuperacaoCompra: boolean
+  SourceCliente: string
+  SessionCustomer_CartProduct: Product[]
 };
 
 export interface ProductData {
