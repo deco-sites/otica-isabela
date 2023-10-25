76a5198cbc64ab675d0e5c5d988d4e7c734bf5bb
igorbrands <igorcantelmo@gmail.com>
2023-10-25 13:41:49 -0300

    feat: filter layout and remove

--- components/search/SelectedFilters.tsx
+++ components/search/SelectedFilters.tsx
@@ -1,5 +1,6 @@
-import type { Filter, FilterToggleValue } from "apps/commerce/types.ts";
 import { isToggle } from "$store/components/search/Filters.tsx";
+import type { Filter, FilterToggleValue } from "apps/commerce/types.ts";
+import Icon from "deco-sites/otica-isabela/components/ui/Icon.tsx";
 
 export interface Props {
   filters: Filter[];
@@ -13,11 +14,14 @@ function SelectedFilters({ filters }: Props) {
   }, []);
 
   return (
-    <ul>
+    <ul class="w-full flex">
       {selected.map((item) => (
-        <li>
-          {item.label}
-        </li>
+        <a href={item.url}>
+          <li class="flex items-center text-base-400 text-base cursor-pointer py-1 px-6 rounded-[5px] mr-5 border border-solid border-base-400">
+            <p>{item.label}</p>
+            <Icon class="text-base-200 ml-2" size={12} id="Close" />
+          </li>
+        </a>
       ))}
     </ul>
   );
