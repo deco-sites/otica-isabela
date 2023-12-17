7c5f04ff67408006fe7fc52e9cb0540ae61b007a
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2023-12-17 10:34:58 -0300

    style: auto format files

--- islands/ApplyRangeFiltersJS.tsx
+++ islands/ApplyRangeFiltersJS.tsx
@@ -1,1 +1,1 @@
-export { default } from 'deco-sites/otica-isabela/components/search/ApplyRangeFiltersJS.tsx'
+export { default } from "deco-sites/otica-isabela/components/search/ApplyRangeFiltersJS.tsx";
