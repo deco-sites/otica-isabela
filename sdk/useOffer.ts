89d1fdb02f6ac79c08fbf49e3bb3fdf1039b85e1
SACCI <saccilucas@gmail.com>
2023-12-19 20:17:22 -0300

    fix(pdpmobile): product installments fixed

--- sdk/useOffer.ts
+++ sdk/useOffer.ts
@@ -5,7 +5,7 @@ import type {
 
 const bestInstallment = (
   acc: UnitPriceSpecification | null,
-  curr: UnitPriceSpecification,
+  curr: UnitPriceSpecification
 ) => {
   if (curr.priceComponentType !== "https://schema.org/Installment") {
     return acc;
@@ -24,7 +24,8 @@ const bestInstallment = (
   }
 
   if (
-    acc.billingDuration && curr.billingDuration &&
+    acc.billingDuration &&
+    curr.billingDuration &&
     acc.billingDuration < curr.billingDuration
   ) {
     return curr;
@@ -35,7 +36,7 @@ const bestInstallment = (
 
 const installmentToString = (
   installment: UnitPriceSpecification,
-  sellingPrice: number,
+  sellingPrice: number
 ) => {
   const { billingDuration, billingIncrement, price } = installment;
 
@@ -45,15 +46,15 @@ const installmentToString = (
 
   const withTaxes = sellingPrice < price;
 
-  return `${billingDuration}x de R$ ${billingIncrement} ${
+  return `${billingDuration}x de R$ ${billingIncrement.toFixed(2)} ${
     withTaxes ? "com juros" : "sem juros"
   }`;
 };
 
 export const useOffer = (aggregateOffer?: AggregateOffer) => {
   const offer = aggregateOffer?.offers[0];
-  const listPrice = offer?.priceSpecification.find((spec) =>
-    spec.priceType === "https://schema.org/ListPrice"
+  const listPrice = offer?.priceSpecification.find(
+    (spec) => spec.priceType === "https://schema.org/ListPrice"
   );
   const installment = offer?.priceSpecification.reduce(bestInstallment, null);
   const seller = offer?.seller;
@@ -65,8 +66,7 @@ export const useOffer = (aggregateOffer?: AggregateOffer) => {
     listPrice: listPrice?.price,
     availability,
     seller,
-    installments: installment && price
-      ? installmentToString(installment, price)
-      : null,
+    installments:
+      installment && price ? installmentToString(installment, price) : null,
   };
 };
