0c17136750608226d3fc9100a8190f6035e46201
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2024-01-05 22:31:35 -0300

    style: auto format file

--- components/product/product-details/Details.tsx
+++ components/product/product-details/Details.tsx
@@ -219,7 +219,10 @@ function Details(
           {/* Buy with lens label */}
           {promotion && (
             <div class="bg-[#a8e3ff] rounded-[2.5px] text-[13px] text-center p-[2.5px] my-[10px] w-[90%] lg:hidden leading-6">
-              {promotion.flagText.replace("%value", formatPrice(price, offers!.priceCurrency!)!)}
+              {promotion.flagText.replace(
+                "%value",
+                formatPrice(price, offers!.priceCurrency!)!,
+              )}
             </div>
           )}
           {/* Dots - Mobile & Desktop */}
