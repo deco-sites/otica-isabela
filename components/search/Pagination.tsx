0410729332c6fed3713d77ec4d98f07ace8aed52
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2024-01-17 14:06:53 -0300

    style: auto format

--- components/search/Pagination.tsx
+++ components/search/Pagination.tsx
@@ -19,13 +19,13 @@ interface PageParams {
 
 export default function Pagination({ pageInfo, breadcrumb }: Props) {
   const { nextPage, previousPage } = pageInfo;
-  console.log(pageInfo)
+  console.log(pageInfo);
   const firstPage = 1;
   const totalPages = Math.ceil(
     Number(pageInfo?.records) / Number(pageInfo?.recordPerPage),
   );
   const moreThanSevenPages = totalPages > firstPage + 6;
-  const pageParams = new URLSearchParams(nextPage ?? previousPage)
+  const pageParams = new URLSearchParams(nextPage ?? previousPage);
 
   const eachPageParams: PageParams[] = Array.from(
     { length: Number(totalPages) },
