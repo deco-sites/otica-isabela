aec31ed6cf71397e911fd043548b79db90d49a24
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-03-06 16:43:07 -0400

    feat: deferred iframe

---
+++ components/ui/DeferredIframe.tsx
@@ -0,0 +1,62 @@
+import { Picture, Source } from "apps/website/components/Picture.tsx";
+import { IImage } from "deco-sites/otica-isabela/sdk/types.ts";
+import { usePartialSection } from "deco/hooks/usePartialSection.ts";
+
+interface Thumbnail {
+  desktop: IImage;
+  mobile: IImage;
+}
+
+interface Props {
+  thumbnail: Thumbnail;
+  iframeUrl: string;
+  container?: boolean;
+  /**
+   * @ignore
+   */
+  showIframe?: boolean;
+}
+
+export default function DeferredIframe({
+  thumbnail,
+  iframeUrl,
+  container = true,
+  showIframe = false,
+}: Props) {
+  return (
+    <div class={"w-full px-5 md:p-0" + (container ? " container" : "")}>
+      {showIframe ? (
+        <iframe
+          class="w-full h-full aspect-video"
+          src={iframeUrl}
+          allowFullScreen
+        />
+      ) : (
+        <button {...usePartialSection<Props>({ props: { showIframe: true } })}>
+          <Picture
+            class="w-full h-full"
+            preload={thumbnail.desktop.preload || thumbnail.mobile.preload}
+          >
+            <Source
+              src={thumbnail.desktop.src}
+              width={thumbnail.desktop.width}
+              height={thumbnail.desktop.height}
+              alt={thumbnail.desktop.alt}
+              fetchPriority={thumbnail.desktop.preload ? "high" : "auto"}
+              loading={thumbnail.desktop.preload ? "eager" : "lazy"}
+            />
+            <Source
+              src={thumbnail.mobile.src}
+              width={thumbnail.mobile.width}
+              height={thumbnail.mobile.height}
+              alt={thumbnail.mobile.alt}
+              fetchPriority={thumbnail.mobile.preload ? "high" : "auto"}
+              loading={thumbnail.mobile.preload ? "eager" : "lazy"}
+            />
+            <img class="object-cover w-full" src={thumbnail.mobile.src} />
+          </Picture>
+        </button>
+      )}
+    </div>
+  );
+}
