511f0495c91bcc2cbea692c568f2ba1857cb8258
jucaserta <jucaserta@hotmail.com>
2025-02-03 22:50:54 -0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -3,5 +3,22 @@ importScripts(
 );
 
 workbox.setConfig({ debug: false });
+
+// Limpa caches antigos durante a instalação
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }),
+  );
+});
+
+// Estratégia de rede apenas (sem cache)
 workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
+
+// Página offline de fallback
 workbox.recipes.offlineFallback({ pageFallback: "/offline" });
