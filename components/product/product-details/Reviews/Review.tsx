9d523c564b625963512c9ee8a51b160709bd2a6b
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2025-04-17 11:10:35 -0300

    fix: review pdp image size

--- components/product/product-details/Reviews/Review.tsx
+++ components/product/product-details/Reviews/Review.tsx
@@ -69,14 +69,14 @@ function Review({ page, productData, memberImages }: Props) {
               {/* Images */}
               <div class="w-full flex flex-col gap-0 md:gap-12 lg:gap-16 ">
                 <div id={id} class="container flex flex-col px-0 sm:px-5">
-                  <Slider class="carousel carousel-center sm:carousel-end gap-0 md:gap-6 col-span-full row-start-2 row-end-5">
+                  <Slider class="carousel carousel-center sm:carousel-end gap-1 md:gap-6 col-span-full row-start-2 row-end-5">
                     {reviews?.map((review, index) => (
                       review.additionalImage &&
                       (
                         <div class="flex flex-col relative">
                           <Slider.Item
                             index={index}
-                            class="carousel-item w-full justify-center"
+                            class="carousel-item !w-full justify-center"
                           >
                             {review.additionalImage
                               ? (
