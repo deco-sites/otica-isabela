f542788060061b978e2d5c064069f5e6bdbc3823
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2025-05-09 10:38:39 -0300

    chore: bump deps

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "$fresh/server.ts";
 import { plugins } from "deco/plugins/deco.ts";
 import partytownPlugin from "partytown/mod.ts";
 import manifest from "./manifest.gen.ts";
-import tailwind from "./tailwind.config.ts";
 
 export default defineConfig({
   plugins: [
     ...plugins({
       manifest,
-      // deno-lint-ignore no-explicit-any
     }),
     partytownPlugin(),
   ],
